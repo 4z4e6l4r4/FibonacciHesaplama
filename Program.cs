8d5c94be5f80fd711f24e958039b6b73626fc1a0
Azra <yukselazrael@gmail.com>
2024-02-08 22:21:07 +0300

    Added Project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FibonacciHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+            Console.WriteLine(fibonacciFuntion(n));
+        }
+
+        static long fibonacciFuntion(int number)
+        {
+            if (number <= 1)
+            {
+                return number;
+            }
+            else
+            {
+                return fibonacciFuntion(number - 1)+fibonacciFuntion(number-2);
+
+            }
+
+        }
+    }
+
+}
